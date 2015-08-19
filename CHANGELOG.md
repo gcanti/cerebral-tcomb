370facbe33901cbaa5fcda4b3683680ecb8b77f6
gcanti <giulio.canti@gmail.com>
2015-08-19 20:09:08 +0200

    v0.2.1

---
+++ CHANGELOG.md
@@ -0,0 +1,27 @@
+# Changelog
+
+> **Tags:**
+> - [New Feature]
+> - [Bug Fix]
+> - [Breaking Change]
+> - [Documentation]
+> - [Internal]
+> - [Polish]
+> - [Experimental]
+
+**Note**: Gaps between patch versions are faulty/broken releases.
+**Note**: A feature tagged as Experimental is in a high state of flux, you're at risk of it changing without notice.
+
+## v0.2.1
+
+- **New Feature**
+  - Checking whether a state path exists #2
+
+## v0.2.0
+
+- **Breaking Change**
+  - upgrade to carebral v0.16
+
+## v0.1.0
+
+Initial release
--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 var tape = require('tape');
 var Controller = require('cerebral');
 var Model = require('../.');
+var validatePath = require('../lib/validatePath')
 var t = Model.t;
 
 var State = t.struct({
@@ -25,14 +26,69 @@ function getController() {
   return Controller(Model(initialState));
 }
 
-tape('controller', function (tape) {
+tape('validatePath', function (tape) {
+
+  tape.test('should issue an error when an invalid path is used', function (assert) {
+    assert.plan(20);
+    var Profile = t.struct({
+      name: t.String
+    });
+    var Person = t.struct({
+      struct: Profile,
+      arr: t.Array,
+      list: t.list(Profile),
+      tuple: t.tuple([t.String, Profile]),
+      dict: t.dict(t.String, Profile),
+      subtype: t.subtype(Profile, function () { return true; }),
+      maybe: t.maybe(Profile),
+      union: t.union([t.String, Profile]),
+      intersection: t.intersection([Profile, Profile])
+    }, 'Person');
+
+    assert.deepEqual(validatePath(['invalid'], Person), ['invalid']);
+
+    assert.strictEqual(validatePath(['struct', 'name'], Person), undefined);
+    assert.deepEqual(validatePath(['struct', 'surname'], Person), ['struct', 'surname']);
+
+    assert.strictEqual(validatePath(['arr', 2], Person), undefined);
+    assert.deepEqual(validatePath(['arr', -1], Person), ['arr', -1]);
+
+    assert.strictEqual(validatePath(['list', 0], Person), undefined);
+    assert.deepEqual(validatePath(['list', -1], Person), ['list', -1]);
+    assert.deepEqual(validatePath(['list', 0, 'surname'], Person), ['list', 0, 'surname']);
+
+    assert.strictEqual(validatePath(['tuple', 0], Person), undefined);
+    assert.deepEqual(validatePath(['tuple', 1, 'surname'], Person), ['tuple', 1, 'surname']);
+
+    assert.strictEqual(validatePath(['dict', 'a'], Person), undefined);
+    assert.deepEqual(validatePath(['dict', 'a', 'surname'], Person), ['dict', 'a', 'surname']);
+
+    assert.strictEqual(validatePath(['subtype', 'name'], Person), undefined);
+    assert.deepEqual(validatePath(['subtype', 'surname'], Person), ['subtype', 'surname']);
+
+    assert.strictEqual(validatePath(['maybe', 'name'], Person), undefined);
+    assert.deepEqual(validatePath(['maybe', 'surname'], Person), ['maybe', 'surname']);
+
+    assert.strictEqual(validatePath(['union', 'name'], Person), undefined);
+    assert.deepEqual(validatePath(['union', 'surname'], Person), ['union', 'surname']);
+
+    assert.strictEqual(validatePath(['intersection', 'name'], Person), undefined);
+    assert.deepEqual(validatePath(['intersection', 'surname'], Person), ['intersection', 'surname']);
+  });
+
+});
+
+tape('model', function (tape) {
 
   tape.test('get', function (assert) {
-    assert.plan(2);
+    assert.plan(3);
     var controller = getController();
-    controller.signal('test', function (input, state, output) {
-      assert.strictEqual(controller.get('email'), 'a@domain.com', 'path as string');
-      assert.strictEqual(controller.get(['email']), 'a@domain.com', 'path as array');
+    controller.signal('test', function (input, state) {
+      assert.strictEqual(state.get('email'), 'a@domain.com', 'path as string');
+      assert.strictEqual(state.get(['email']), 'a@domain.com', 'path as array');
+      assert.throws(function () {
+        state.get(['profile', 'name']);
+      });
     });
     controller.signals.test();
   });
@@ -40,7 +96,7 @@ tape('controller', function (tape) {
   tape.test('set', function (assert) {
     assert.plan(3);
     var controller = getController();
-    controller.signal('test', function (input, state, output) {
+    controller.signal('test', function (input, state) {
       state.set('email', 'b@domain.com', 'path as string');
       assert.strictEqual(controller.get('email'), 'b@domain.com');
       state.set(['profile', 'age'], 42, 'path as array');
@@ -55,7 +111,7 @@ tape('controller', function (tape) {
   tape.test('unset', function (assert) {
     assert.plan(2);
     var controller = getController();
-    controller.signal('test', function (input, state, output) {
+    controller.signal('test', function (input, state) {
       state.unset('email');
       assert.strictEqual(controller.get('email'), null, 'path as string');
       state.unset(['profile', 'age']);
@@ -67,7 +123,7 @@ tape('controller', function (tape) {
   tape.test('concat', function (assert) {
     assert.plan(1);
     var controller = getController();
-    controller.signal('test', function (input, state, output) {
+    controller.signal('test', function (input, state) {
       state.concat('tags', ['rock climber', 'SF reader']);
       assert.deepEqual(controller.get('tags'), ['web developer', 'rock climber', 'SF reader']);
     });
@@ -77,7 +133,7 @@ tape('controller', function (tape) {
   tape.test('push', function (assert) {
     assert.plan(1);
     var controller = getController();
-    controller.signal('test', function (input, state, output) {
+    controller.signal('test', function (input, state) {
       state.push('tags', 'rock climber');
       assert.deepEqual(controller.get('tags'), ['web developer', 'rock climber']);
     });
@@ -87,7 +143,7 @@ tape('controller', function (tape) {
   tape.test('splice', function (assert) {
     assert.plan(1);
     var controller = getController();
-    controller.signal('test', function (input, state, output) {
+    controller.signal('test', function (input, state) {
       state.set('tags', ['web developer', 'rock climber', 'SF reader'])
       state.splice('tags', [1, 1], [1, 1, 'bass player']);
       assert.deepEqual(controller.get('tags'), ['web developer', 'bass player']);
@@ -98,7 +154,7 @@ tape('controller', function (tape) {
   tape.test('merge', function (assert) {
     assert.plan(2);
     var controller = getController();
-    controller.signal('test', function (input, state, output) {
+    controller.signal('test', function (input, state) {
       state.merge(['other'], {b: 2});
       assert.deepEqual(controller.get('other'), {a: 1, b: 2});
       state.merge(['other', 'a'], {c: 3});
@@ -110,7 +166,7 @@ tape('controller', function (tape) {
   tape.test('pop', function (assert) {
     assert.plan(1);
     var controller = getController();
-    controller.signal('test', function (input, state, output) {
+    controller.signal('test', function (input, state) {
       state.pop('tags');
       assert.deepEqual(controller.get('tags'), []);
     });
@@ -120,7 +176,7 @@ tape('controller', function (tape) {
   tape.test('shift', function (assert) {
     assert.plan(1);
     var controller = getController();
-    controller.signal('test', function (input, state, output) {
+    controller.signal('test', function (input, state) {
       state.shift('tags');
       assert.deepEqual(controller.get('tags'), []);
     });
@@ -130,7 +186,7 @@ tape('controller', function (tape) {
   tape.test('unshift', function (assert) {
     assert.plan(1);
     var controller = getController();
-    controller.signal('test', function (input, state, output) {
+    controller.signal('test', function (input, state) {
       state.unshift('tags', 'rock climber');
       assert.deepEqual(controller.get('tags'), ['rock climber', 'web developer']);
     });
